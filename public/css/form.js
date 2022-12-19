e58f68bb0c629de84eb7ccc3ce28cec242d925db
ArchiPatel8 <archi.patel.1501@gmail.com>
2022-12-18 22:25:26 -0500

    updated layout and handlebars

--- public/css/form.js
+++ public/css/form.js
@@ -1,19 +1,20 @@
-function myStartFunction(){
-    document.getElementById("start-button-final").className = "btn btn-secondary"
-    // document.getElementById("start-button-final").disvbled = true;
-    document.getElementById("finish-button-final").className = "btn btn-secondary"
-    // document.getElementById("finish-button-final").disabled = true;
+// function myStartFunction(){
+//     document.getElementById("start-button-final").style.visibility = 'hidden';
+//     // document.getElementById("start-button-final").style.display = "none";
+//     // document.getElementById("start-button-final").disvbled = true;
+//     document.getElementById("finish-button-final").style.visibility = 'hidden';
+//     // document.getElementById("finish-button-final").disabled = true;
     
-}
+// }
 
-function myStopFunction(){
-    document.getElementById("stop-button-final").disabled = true;
-    document.getElementById("start-button-final").disabled = false;
-    document.getElementById("finish-button-final").disabled = false;
-}
+// function myStopFunction(){
+//     document.getElementById("stop-button-final").style.visibility = 'hidden';
+//     document.getElementById("start-button-final").style.visibility = 'hidden';
+//     document.getElementById("finish-button-final").style.visibility = 'hidden';
+// }
 
-function myFinishFunction(){
-    document.getElementById("stop-button-final").disabled = true;
-    document.getElementById("start-button-final").disabled = true;
-    document.getElementById("finish-button-final").disabled = true;
-}
+// function myFinishFunction(){
+//     document.getElementById("stop-button-final").disabled = true;
+//     document.getElementById("start-button-final").disabled = true;
+//     document.getElementById("finish-button-final").disabled = true;
+// }
